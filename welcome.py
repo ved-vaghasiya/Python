065e5a9cadd47e6c89446a572bd48b693708e8dd
Ved-Vaghasiya <ved.vaghasiya.cg@gmail.com>
2026-08-21 22:31:53 +0530

    changes has been done

---
+++ welcome.py
@@ -0,0 +1,6 @@
+print("hello world")
+x= input("enter your name :")
+age=input("enter your age :")
+education = input("enter your education :")
+print("HI Ved welcomes you")
+#this your small program to welcome you 
