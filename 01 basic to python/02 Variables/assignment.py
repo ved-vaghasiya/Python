f074872001a719c6b7d0f9cc06ed34ea09be2508
Ved-Vaghasiya <ved.vaghasiya.cg@gmail.com>
2026-08-23 19:00:41 +0530

    changes done

---
+++ 01 basic to python/02 Variables/assignment.py
@@ -0,0 +1,33 @@
+#24
+name = "ved"
+age=18
+city="surat"
+print(name,age,city)
+
+#25
+student_name = "ved vaghasiya"
+student_roll=11223690234
+student_branch="cse"
+print(student_name,student_roll,student_branch)
+
+#26
+marks=35
+marks=40
+print(marks)
+
+#27
+name="rahul"
+age=18
+city= "patna"
+print(name,age,city)
+
+#28
+x=y=z=0
+print(x,y,z)
+
+#29
+student1 = "Rahul"
+student_name1 = "Rahul"
+class1 ="B.Tech"
+print(student1,student_name1,class1)
+    
