ea51d0b4269e38c413a0d12073ae575badeec66c
Ved-Vaghasiya <ved.vaghasiya.cg@gmail.com>
2026-08-25 11:06:21 +0530

    changes done

--- 03 Data_types and Type_casts/classwork.py
+++ 03 Data_types and Type_casts/classwork.py
@@ -1,11 +1,37 @@
-age = 18
-name = "ved vaghasiya"
-age_float = 18.0
-age_boolean = True
-result_nonetype= None
-print(age,name,age_float,age_boolean)
-print(type(age))
-print(type(name))
-print(type(age_float))
-print(type(age_boolean))
-print(type(result_nonetype))
+# age = 18
+# name = "ved vaghasiya"
+# age_float = 18.0
+# age_boolean = True
+# result_nonetype= None
+# print(age,name,age_float,age_boolean)
+# print(type(age))
+# print(type(name))
+# print(type(age_float))
+# print(type(age_boolean))
+# print(type(result_nonetype))
+
+
+
+####################### Arthmetic Operartor
+
+a=10
+b=5
+print (a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a*b)
+print(a**b)
+print(a*3)
+
+x=-10
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**4)
+print(x%y)
+print(x-(-y))
+
+
+
