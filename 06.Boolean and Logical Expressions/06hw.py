79529dffbac791ced0b8ef16bc9a51353e2a1cd1
Ved-Vaghasiya <ved.vaghasiya.cg@gmail.com>
2026-09-04 09:35:26 +0530

    Add files via upload

---
+++ 06.Boolean and Logical Expressions/06hw.py
@@ -0,0 +1,66 @@
+#6......
+print(10==10)
+
+#7..
+print(10 != 5)
+
+#8...
+print(5 > 10)
+
+#9.....
+print(5 < 10)
+
+#10...
+print(18 >= 18)
+
+#11....
+print(18 <= 10)
+
+#15.....
+age = 20
+print(age >= 18)
+
+#16.......
+marks = 45
+print(marks == 50)
+
+#18.....
+print(1 and 1)
+print(1 and 0)
+print( 0 and 4 )
+print("" and 0)
+
+#19.....
+print(1 or 1)
+print(1 or 0)
+print( 0 or 4 )
+print("" or 0)
+
+#20.....
+print(not 1 )
+print(not 0)
+
+#22.....
+age = 25
+
+print(age >= 18 and age <= 60)
+
+#23.....
+age = 16
+
+print(age < 18 or age > 60)
+
+#24....
+age = 20
+
+print(not age < 18)
+
+#25......
+number = 25
+
+print(number > 10 and number < 50)
+
+#26....
+number = 150
+
+print(number < 10 or number > 100)
