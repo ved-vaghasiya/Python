8c3ef5ad0ec72634fdec320d8db79e54b2356bf9
Ved-Vaghasiya <ved.vaghasiya.cg@gmail.com>
2026-09-05 09:35:10 +0530

    Add files via upload

---
+++ 07 Input And Output/07cw.py
@@ -0,0 +1,56 @@
+# name=input("enter your full name:")
+# print(f"your  name: {name} was recorded")
+
+# age=input("enter your age :")
+# print(f"your  age: {age} was recorded")
+
+# lab=input("enter your lab:")
+# print(f"your lab: {lab} was recorded")
+
+# product_name=input("enter your product_name:")
+# print(f"your  product_name: {product_name} was recorded")
+
+# product_price=input("enter your product_pric:")
+# print(f"your  product_price: {product_price} was recorded")
+
+# product_count=input("enter your product_count:")
+# print(f"your  product_count: {product_count} was recorded")
+
+# total=int(product_price)*int(product_count)
+# print(f"total is {total}")
+
+
+# print("code is over :::")
+
+# a,b=map(int,input("enter two numbers:").split())
+# print(a,b)
+# print(a*b)
+
+# x,y=(input("enter two numbers:").split()[:2])
+# print(f"first_number: {x} second_number: {y}")
+
+
+#seprator
+# print("hello","ved","vaghasiya" ,sep="_"[:1])   
+# print("hello",end="")
+# print("world")
+
+
+
+
+
+#########################################CONITIONAL STATEMNTS#####################################################################################
+result=float(input("enter your result :"))
+if(result>=33):
+    print("pass")
+else:
+    print("fail")    
+
+
+number=int(input("Enter a number:"))
+
+if number%2==0:
+    print("number is even!!! ")
+
+if number%2==1 :
+    print("number is odd !!!!")
